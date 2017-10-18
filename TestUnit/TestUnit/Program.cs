00dd3b92064755178de4700b4171f8b16cb8df1b
Ludovic Merel <Ludo.m@live.fr>
2017-10-18 15:36:47 +0200

    Update test unit

--- TestUnit/TestUnit/Program.cs
+++ TestUnit/TestUnit/Program.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using Gestion_Entreprise;
 
 namespace TestUnit
 {
-    class Program
+    [TestFixture()]
+    public class Employee
     {
-        static void Main(string[] args)
-        {
-        }
+        Assert.That();
     }
 }
